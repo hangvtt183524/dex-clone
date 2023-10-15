6a95fb0705da082871994721cda8663fdb0e9eba
hangvtt183524 <nanh.trang.140@gmail.com>
2023-10-15 17:32:23 +0700

    ..

--- src/svgs/icons/CheckMarkGradient.tsx
+++ src/svgs/icons/CheckMarkGradient.tsx
@@ -3,7 +3,7 @@ import Svg from 'components/Svg/Svg';
 import { SvgProps } from 'components/Svg/types';
 import React from 'react';
 
-const CheckMarkGradient: React.FC<React.PropsWithChildren<SvgProps>> = (props) => {
+const CheckMarkGradient  = (props) => {
   return (
     <Svg viewBox="0 0 20 20" fill="transparent" {...props}>
       <path
